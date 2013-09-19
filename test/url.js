2604a786ce39aadda07c84f4c7e7034fe2b5bea8
Jacob Rask <jacob@jacobrask.net>
2013-09-19 19:39:42 +0200

    Add tests

---
+++ test/url.js
@@ -0,0 +1,36 @@
+var test = require('tap').test;
+var urlsFrom = require('../');
+
+test('Get URLs', function (t) {
+  t.plan(1);
+  
+  var urlStream = urlsFrom();
+  urlStream.on('data', function (url) {
+    t.equal(url.toString(), 'http://example.com');
+  });
+  urlStream.write('Lorem ipsum http://example.com');
+  urlStream.end();
+});
+
+test('Get URL objects', function (t) {
+  t.plan(1);
+  
+  var urlStream = urlsFrom({ objectMode: true });
+  urlStream.on('data', function (url) {
+    t.equal(url.href, 'http://example.com');
+  });
+  urlStream.write('Lorem ipsum http://example.com');
+  urlStream.end();
+});
+
+test('Get validated URLs', function (t) {
+  t.plan(1);
+  
+  var urlStream = urlsFrom({ validate: true });
+  urlStream.on('data', function (url) {
+    t.equal(url.toString(), 'http://example.com');
+  });
+  urlStream.write('Lorem ipsum http://example.com');
+  urlStream.end();
+});
+
